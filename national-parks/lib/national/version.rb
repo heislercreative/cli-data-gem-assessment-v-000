7034f874157d5db6a273f26ae0886480660d0949
Brennan Heisler <learn@heislercreative.com>
2018-03-13 12:03:23 +0000

    automatically backed up by learn

--- national-parks/lib/national/version.rb
+++
@@ -1,5 +0,0 @@
-module National
-  module Parks
-    VERSION = "0.1.0"
-  end
-end
--- national-parks/lib/national_parks.rb
+++
@@ -1,7 +0,0 @@
-require "national/parks/version"
-
-module National
-  module Parks
-    # Your code goes here...
-  end
-end
